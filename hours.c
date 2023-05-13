a4f3849f33cc2eafe8e2423e9c53e0dc02b95b79
TomasNagy <tomi.nagy@outlook.com>
2023-05-13 13:14:10 +0200

    Add hours.c

---
+++ hours.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// Get user input for the number of weeks
+
+	// Get the learned hours for each week
+
+	// Ask the user which information he wants to calculate (A - average hours, T - total hours) 
+
+	// Calculate and print result
+}
