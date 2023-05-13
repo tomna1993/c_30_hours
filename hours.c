23ac061d16940b221eb9355b393b74d0ccb7e5d7
TomasNagy <tomi.nagy@outlook.com>
2023-05-13 13:57:57 +0200

    First working implementation

--- hours.c
+++ hours.c
@@ -1,20 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void calculate_total(int num_array[]);
-void calculate_average(int num_array[]);
+void calculate_total(int num_array[], const int length);
+void calculate_average(int num_array[], const int length);
 
 int main(void)
 {
 	// Get user input for the number of weeks
+	// use constant integer to store the numbers of weeks -> there is no chance that 
+	// somewhere the program can change its value
 	const int weeks_learned = get_int("Numbers of weeks taking CS50: ");
 
+	// create a dynamic array to store the hours
 	int hours_learned[weeks_learned];
 
 	// Get the learned hours for each week
 	for(int i = 0; i < weeks_learned; i++)
 	{
-		hours_learned[i] = get_int("Week %i HW Hours: ");
+		hours_learned[i] = get_int("Week %i HW Hours: ", i);
 	}
 
 	// Ask the user which information he wants to calculate (A - average hours, T - total hours) 
@@ -23,20 +26,45 @@ int main(void)
 	// Calculate and print result
 	if(calculation_mode == 'T')
 	{
-		calculate_total(hours_learned);
+		calculate_total(hours_learned, weeks_learned);
 	}
 	else if(calculation_mode == 'A')
 	{
-		calculate_average(hours_learned);
+		calculate_average(hours_learned, weeks_learned);
 	}
-	
+
+	// exit status 0, the program has run successfully	
 	return 0;
 }
 
-void calculate_total(int num_array[])
+// Function to calculate the total amount of hours spent with studying and print out the sum
+void calculate_total(int num_array[], int length)
 {
+	// declare a variable and initialize it to 0
+	int total = 0;
+
+	// iterate through the elements of the array and add them together
+	for(int i = 0; i < length; i++)
+	{
+		total += num_array[i];
+	}
+
+	// print out the sum of the array
+	printf("%i hours\n", total);
 }
 
-void calculate_average(int num_array[])
+// Function to calculate the average of hours spent with studying and print out the result 
+void calculate_average(int num_array[], int length)
 {
+	// declare a variable and initialize it to 0
+	int total = 0;
+
+	// iterate through the elements of the array and add them together
+	for(int i = 0; i < length; i++)
+	{
+		total += num_array[i];
+	}
+
+	// calculate the average and print it out
+	printf("%.1f hours\n", total / (float) length);
 }
