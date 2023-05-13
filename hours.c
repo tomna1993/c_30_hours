165f407afbf959680149bba3d54f7c497c55dd8d
TomasNagy <tomi.nagy@outlook.com>
2023-05-13 20:46:53 +0200

    Create user input functions and more feedback to user

--- hours.c
+++ hours.c
@@ -1,6 +1,10 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 
+int get_weeks_learned(void);
+void get_learned_hours(int array_hours_learned[], int num_weeks_learned);
+char get_calculation_mode(void);
 void calculate_total(int num_array[], const int length);
 void calculate_average(int num_array[], const int length);
 
@@ -9,32 +13,91 @@ int main(void)
 	// Get user input for the number of weeks
 	// use constant integer to store the numbers of weeks -> there is no chance that 
 	// somewhere the program can change its value
-	const int weeks_learned = get_int("Numbers of weeks taking CS50: ");
+	const int weeks_learned = get_weeks_learned();
 
 	// create a dynamic array to store the hours
 	int hours_learned[weeks_learned];
 
 	// Get the learned hours for each week
-	for(int i = 0; i < weeks_learned; i++)
-	{
-		hours_learned[i] = get_int("Week %i HW Hours: ", i);
-	}
+	get_learned_hours(hours_learned, weeks_learned);
 
 	// Ask the user which information he wants to calculate (A - average hours, T - total hours) 
-	char calculation_mode = get_char("Enter T for total hours, A for average hours per week: ");
+	char calculation_mode = get_calculation_mode(); 
 
 	// Calculate and print result
 	if(calculation_mode == 'T')
 	{
 		calculate_total(hours_learned, weeks_learned);
 	}
-	else if(calculation_mode == 'A')
+	else
 	{
 		calculate_average(hours_learned, weeks_learned);
 	}
 
 	// exit status 0, the program has run successfully	
 	return 0;
+}
+
+// Get number of weeks learned from user 
+int get_weeks_learned(void)
+{
+	int number;
+
+	// Ask again for a number if user inputs characters, strings or negative nuber	
+	do	
+	{
+		number = get_int("Numbers of weeks taking CS50: ");
+	}
+	while (number < 1);
+
+	return number;
+}
+
+// Get how much time the user spent with learning
+void get_learned_hours(int array_hours_learned[], int num_weeks_learned)
+{
+	int hours;
+
+	// Loop through the weeks and save learned hours
+	for(int i = 0; i < num_weeks_learned; i++)
+	{
+		do
+		{
+			// Get user input
+			hours = get_int("Week %i HW Hours: ", i+1);
+
+			// Feedback to user, it's not possible to learn so much in a week
+			if(hours > 100)
+			{
+				printf("You're kidding! No human can learn and focus that much! Try with another lie ;D\n");
+			}
+			// It's impossible to unlearn something or learn less hours than 0
+			else if(hours < 0)
+			{
+				printf("Wow, impressive! How can you unlearn something?\n");
+			}
+		
+		} while (hours <= 0 || hours > 100);
+		// Save learned hours
+		array_hours_learned[i] = hours;
+	}	
+}
+
+// Get calculation mode from user
+char get_calculation_mode(void)
+{
+	char input; 
+
+	// ask for input mode until user enters 'T', 't', 'A', 'a'
+	do
+	{
+		input = get_char("Enter T for total hours, A for average hours per week: ");
+		// convert lowercase letter to uppercase
+		input = toupper(input);
+
+	} while (input != 'T' && input != 'A');
+
+	return input;		
 }
 
 // Function to calculate the total amount of hours spent with studying and print out the sum
