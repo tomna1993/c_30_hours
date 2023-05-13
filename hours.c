0706856e6585cbe15f0b7b7f37c5623a3e1f1bdd
TomasNagy <tomi.nagy@outlook.com>
2023-05-13 13:32:25 +0200

    Add function declarations, user inputs

--- hours.c
+++ hours.c
@@ -1,13 +1,42 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void calculate_total(int num_array[]);
+void calculate_average(int num_array[]);
+
 int main(void)
 {
 	// Get user input for the number of weeks
+	const int weeks_learned = get_int("Numbers of weeks taking CS50: ");
+
+	int hours_learned[weeks_learned];
 
 	// Get the learned hours for each week
+	for(int i = 0; i < weeks_learned; i++)
+	{
+		hours_learned[i] = get_int("Week %i HW Hours: ");
+	}
 
 	// Ask the user which information he wants to calculate (A - average hours, T - total hours) 
+	char calculation_mode = get_char("Enter T for total hours, A for average hours per week: ");
 
 	// Calculate and print result
+	if(calculation_mode == 'T')
+	{
+		calculate_total(hours_learned);
+	}
+	else if(calculation_mode == 'A')
+	{
+		calculate_average(hours_learned);
+	}
+	
+	return 0;
+}
+
+void calculate_total(int num_array[])
+{
+}
+
+void calculate_average(int num_array[])
+{
 }
